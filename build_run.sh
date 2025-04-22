d0059d8d2840d08f82e225344fd2042289f05a72
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-04-22 10:33:39 +0700

    Make src dir

--- build_run.sh
+++ build_run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 packages=(sll database)
+cd src
 
 for package in "${packages[@]}"; do
-    echo "COMPILING AND RUNNING "$package" PACKAGE..."
+    echo "COMPILING AND RUNNING '"$package"' PACKAGE..."
     javac "$package"/Main.java
     java "$package".Main
 done
