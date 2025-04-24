420389a2f9ac8da03721203946dd6801e86a208d
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-04-25 00:54:57 +0700

    Add copyright

--- text-editor/src/com/zhrsh/texteditor/Color.java
+++ text-editor/src/com/zhrsh/texteditor/Color.java
@@ -1,14 +1,16 @@
+/* Copyright (c) 2025 Zahra A. S. */
+
 package com.zhrsh.texteditor;
 
 /**
  * Kelas Color menyediakan metode untuk print pesan ke konsol dengan warna yang berbeda.
- * Kelas ini menggunakan kode ANSI untuk mengubah warna teks yang dicetak.
+ * Kelas ini menggunakan kode ANSI untuk mengubah warna teks yang print.
  */
 public class Color {
     /**
      * Print pesan dengan warna merah diikuti dengan newline.
      *
-     * @param message Pesan yang akan dicetak.
+     * @param message Pesan yang akan print.
      */
     public static void printRedln(String message) {
         System.out.println("\033[31m" + message + "\033[0m");
@@ -17,7 +19,7 @@ public class Color {
     /**
      * Print pesan dengan warna kuning diikuti dengan newline.
      *
-     * @param message Pesan yang akan dicetak.
+     * @param message Pesan yang akan print.
      */
     public static void printYellowln(String message) {
         System.out.println("\033[93m" + message + "\033[0m");
@@ -26,7 +28,7 @@ public class Color {
     /**
      * Print pesan dengan warna kuning tanpa newline.
      *
-     * @param message Pesan yang akan dicetak.
+     * @param message Pesan yang akan print.
      */
     public static void printYellow(String message) {
         System.out.print("\033[93m" + message + "\033[0m");
@@ -35,7 +37,7 @@ public class Color {
     /**
      * Print pesan dengan warna abu-abu tanpa newline.
      *
-     * @param message Pesan yang akan dicetak.
+     * @param message Pesan yang akan print.
      */
     public static void printGrey(String message) {
         System.out.print("\033[90m" + message + "\033[0m");
--- text-editor/src/com/zhrsh/texteditor/FileIO.java
+++ text-editor/src/com/zhrsh/texteditor/FileIO.java
@@ -1,3 +1,5 @@
+/* Copyright (c) 2025 Zahra A. S. */
+
 package com.zhrsh.texteditor;
 
 import java.nio.file.Files;
