00c12a2374e1def8a2f35cd282c80d198eb690dc
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-05-19 17:15:00 +0700

    Add employeemanagement script

---
+++ employee-management/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+packages=(employeemanagement) # array of packages to compile
+main_class="Main" # main class name
+
+cd src
+
+for package in "${packages[@]}"; do
+    echo "COMPILING AND RUNNING '"$package"' PACKAGE..."
+    javac com/zhrsh/"$package"/"$main_class".java
+    java com.zhrsh."$package"."$main_class" "$@" # accept command line args
+done
