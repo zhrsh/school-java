7e38fef9aaa818ce1c9742b8898c6b66927b300a
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-05-20 12:44:56 +0700

    Rename evalutation project folder

--- DSA-EVAL/build.sh
+++ DSA-EVAL/build.sh
@@ -8,5 +8,5 @@ cd src
 for package in "${packages[@]}"; do
     echo "COMPILING AND RUNNING '"$package"' PACKAGE..."
     javac com/zhrsh/"$package"/"$main_class".java
-    java com.zhrsh."$package"."$main_class" "$@" # accept command line args
+    java com.zhrsh."$package"."$main_class"
 done
