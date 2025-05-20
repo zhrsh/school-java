7abe38b6c48bc60661a971285c410787f7c6a3bf
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-05-21 03:06:23 +0700

    Add search interface in bst

--- DSA-EVAL/src/io/github/zhrsh/bst/Main.java
+++ DSA-EVAL/src/io/github/zhrsh/bst/Main.java
@@ -1,16 +1,32 @@
 package io.github.zhrsh.bst;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
 
+        // insert integer sesuai permintaan
         int[] values = {15, 17, 12, 4, 17, 9, 2, 20, 21, 27};
         for (int value : values) {
             bst.insert(value);
         }
 
+        // traversal inorder
         System.out.println("inorder traversal dari BST:");
         bst.inorder();
+
+        // search
+        System.out.print("search BST: ");
+        Scanner scanner = new Scanner(System.in);
+        int intToSearch = scanner.nextInt();
+        if (bst.search(intToSearch)) {
+            System.out.println("data exists");
+        }
+        else {
+            System.out.println("data does not exist");
+        }
+        scanner.close();
     }
 }
 
