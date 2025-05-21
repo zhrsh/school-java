79046a002584defaaf1b64b34f6ddc955d46cbd1
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-05-21 20:36:27 +0700

    Update build.sh

--- DSA-EVAL/build.sh
+++ DSA-EVAL/build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-packages=(employeemanagement sll bst) # array of packages to compile
+packages=(employeemanagement sll bst hashtable) # array of packages to compile
 main_class="Main" # main class name
+
+length=${#packages[@]}
+index=1
 
 cd src
 
 for package in "${packages[@]}"; do
-    echo "COMPILING AND RUNNING 'io.github.zhrsh."$package"' PACKAGE..."
+    echo "("$index"/"$length")" "COMPILING AND RUNNING 'io.github.zhrsh."$package"' PACKAGE..."
     javac io/github/zhrsh/"$package"/"$main_class".java
     java io.github.zhrsh."$package"."$main_class" "$@"
+    ((index++))
 done
