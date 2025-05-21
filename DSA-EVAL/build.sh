1331aae9adfed1ca500633ddaad1d54f79fadb14
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-05-21 20:59:21 +0700

    Add error handling to build.sh

--- DSA-EVAL/build.sh
+++ DSA-EVAL/build.sh
@@ -6,11 +6,29 @@ main_class="Main" # main class name
 length=${#packages[@]}
 index=1
 
+if [[ $(pwd) != *DSA-EVAL ]]; then
+    echo "you don't seem to be in the correct directory. run anyway? (y/n)"
+    
+    # read user input
+    read -r answer
+    
+    if [[ $answer == "y" || $answer == "Y" ]]; then
+        echo "running the script..."
+    elif [[ $answer == "n" || $answer == "N" ]]; then
+        echo "exiting the script..."
+        exit 0
+    else
+        echo "invalid input. please enter 'y' or 'n'."
+        exit 0
+    fi
+fi
+
 cd src
 
 for package in "${packages[@]}"; do
+    echo ""
     echo "("$index"/"$length")" "COMPILING AND RUNNING 'io.github.zhrsh."$package"' PACKAGE..."
     javac io/github/zhrsh/"$package"/"$main_class".java
-    java io.github.zhrsh."$package"."$main_class" "$@"
+    java io.github.zhrsh."$package"."$main_class"
     ((index++))
 done
