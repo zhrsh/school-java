dfac47d4e3af3f59cfd21b492ed0bec4e36be49a
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-05-22 01:08:21 +0700

    Add displayDebug attribute to display hashing process

--- DSA-EVAL/src/io/github/zhrsh/hashtable/HashTable.java
+++ DSA-EVAL/src/io/github/zhrsh/hashtable/HashTable.java
@@ -6,11 +6,13 @@ import java.util.LinkedList;
 public class HashTable {
     private LinkedList<String>[] table;
     private int size;
+    private boolean displayDebug; // untuk menampilkan proses hash function
 
     // constructor
     @SuppressWarnings("unchecked")
-    public HashTable(int size) {
+    public HashTable(int size, boolean displayDebug) {
         this.size = size;
+        this.displayDebug = displayDebug;
         table = new LinkedList[size];
         for (int i = 0; i < size; i++) {
             table[i] = new LinkedList<>();
@@ -18,9 +20,20 @@ public class HashTable {
     }
 
     // hash function
+    // jika attribute debug bernilai true, maka metode ini akan mencetak informasi tentang proses hashing, 
+    // termasuk string yang di-hash, nilai panjangnya, dan hasil akhir dari operasi modulo.
     private int hash(String name) {
-        return name.length() % size; // custom hash function sesuai diagram
-        // return Math.abs(name.hashCode()) % size; // hash function lebih efisien
+        int hashedString = name.length(); // custom hash function sesuai diagram
+        // int hashedString = Math.abs(name.hashCode()); // hash function lebih efisien
+        
+        int hashedStringRound = hashedString % size;
+        if (displayDebug) {
+            System.out.printf(
+                "%-7s ->  %s  ->  %s %% 10 = %s%n",
+                name, hashedString, hashedString, hashedStringRound
+            );
+        }
+        return hashedStringRound;
     }
 
     // insert method
--- DSA-EVAL/src/io/github/zhrsh/hashtable/Main.java
+++ DSA-EVAL/src/io/github/zhrsh/hashtable/Main.java
@@ -2,11 +2,14 @@ package io.github.zhrsh.hashtable;
 
 public class Main {
     public static void main(String[] args) {
-        HashTable hashTable = new HashTable(10);
+        HashTable hashTable = new HashTable(10, true);
         
+        // array nama yang diminta
         String[] names = {"Andi", "Ingrit", "Dora", "Chris", "Della", "Jeremy", "Jessica", "Bella", "Emma", "Blake"};
+        
+        // insert nama dalam array names
+        System.out.println("\n=== PROSES HASHING ===");
         for (String name : names) {
-            // insert nama dalam array names
             hashTable.insert(name);
         }
 
